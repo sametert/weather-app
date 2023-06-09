71ae39c3abeb300f879e085a61b6918dcc7a2f09
comolokko <samederturk55@gmail.com>
2023-06-09 22:58:30 +0300

    checked

---
+++ script.js
@@ -0,0 +1,37 @@
+const api = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "71faecb0a31d8450c95f9b081dc436be";
+const temp = document.querySelector(".temp");
+const city = document.querySelector(".city");
+const desc = document.querySelector(".desc");
+const minmax = document.querySelector(".minmax");
+
+const setQuery = e => {
+    if (e.keyCode == "13") {
+        getResult(input.value);
+    }
+}
+
+const getResult = cityName => {
+    let query = `${api}${cityName}&appid=${apiKey}&units=metric&lang=tr`;
+
+    fetch(query)
+        .then(response => response.json())
+        .then(data => {
+            displayResult(data);
+        })
+        .catch(() => alert("Hatalı veya eksik şehir ismi girdiniz."))
+
+
+    const displayResult = veri => {
+        city.innerHTML = veri.name;
+        temp.innerHTML = Math.floor(veri.main.temp) + "°C";
+        desc.innerHTML = veri.weather[0].description;
+        minmax.innerHTML = `${Math.floor(veri.main.temp_min)}°C / ${Math.floor(veri.main.temp_max)}°C`;
+    }
+}
+
+
+
+//first step
+const input = document.querySelector("#searchBar");
+input.addEventListener("keypress", setQuery);
